07682501738feea153fbffe2a550b56a1fca9fff
justinjskootskyuser <justinjskootskyuser@gmail.com>
2026-06-14 11:22:55 -0700

    estate_property table is created in the real-esate-app [sic] database

---
+++ odoo/addons/estate/models/estate_property.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class Property(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate Properties"
+
+    
