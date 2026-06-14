6f4556e80db4a43262ca59992a94c6fac3e7ade1
justinjskootskyuser <justinjskootskyuser@gmail.com>
2026-06-14 10:30:30 -0700

    added init and manifest .py files for real esate app. app shows in the app directory and is activated on the database

---
+++ odoo/addons/estate/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Real Estate',
+    'version': '1.0',
+    'summary': 'Buy and sell houses',
+    'depends': [
+        'base_setup',
+    ],
+    'application': True,
+}
