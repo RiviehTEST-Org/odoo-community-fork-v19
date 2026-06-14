75b8c451df959b62614aff0e778ff30fa35bb308
justinjskootskyuser <justinjskootskyuser@gmail.com>
2026-06-14 11:48:20 -0700

    fields added to estate_property model with name and expected_price fields required

--- odoo/addons/estate/models/estate_property.py
+++ odoo/addons/estate/models/estate_property.py
@@ -8,4 +8,19 @@ class Property(models.Model):
     _name = "estate.property"
     _description = "Real Estate Properties"
 
-    
+    name = fields.Char('Name of Property', required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float('Listed Price', required=True)
+    selling_price = fields.Float('Price Sold')
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string='Garden Orientation',
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        help="Specify the direction the garden faces according to azimuth compass points")
