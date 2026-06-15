31cf4944b7ab087fdbb08f8b9ce3d5f557f83fb0
justinjskootskyuser <justinjskootskyuser@gmail.com>
2026-06-14 18:48:45 -0700

    modified basic fields to include default, read-only, and copy behavior. also added two reserved fields

--- odoo/addons/estate/models/estate_property.py
+++ odoo/addons/estate/models/estate_property.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields, models
+from dateutil.relativedelta import relativedelta
 
 
 class Property(models.Model):
@@ -9,18 +10,25 @@ class Property(models.Model):
     _description = "Real Estate Properties"
 
     name = fields.Char('Name of Property', required=True)
-    description = fields.Text()
-    postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float('Listed Price', required=True)
-    selling_price = fields.Float('Price Sold')
-    bedrooms = fields.Integer()
-    living_area = fields.Integer()
-    facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
+    description = fields.Text('Description')
+    postcode = fields.Char('Postcode')
+    date_availability = fields.Date('Available From', copy=False, default=fields.Date.today() + relativedelta(months=3))
+    expected_price = fields.Float('List Price', required=True)
+    selling_price = fields.Float('Price Sold', readonly=True, copy=False)
+    bedrooms = fields.Integer('Bedrooms', default=2)
+    living_area = fields.Integer('Living Area (sqm)')
+    facades = fields.Integer('Facades')
+    garage = fields.Boolean('Garage')
+    garden = fields.Boolean('Garden')
+    garden_area = fields.Integer('Garden Area (sqm)')
     garden_orientation = fields.Selection(
         string='Garden Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
-        help="Specify the direction the garden faces according to azimuth compass points")
+        help="Specify the direction the garden is facing")
+    active = fields.Boolean('Active Listing', default=True)
+    states = fields.Selection(
+        string='Listing Stage',
+        selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'),
+                   ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        default='new',
+        help="The status of the property listing")
