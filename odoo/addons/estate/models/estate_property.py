f14881692fe39a01b7f8c4eb68b6445e3d8a0f5f
justinjskootskyuser <justinjskootskyuser@gmail.com>
2026-06-15 15:44:31 -0700

    added 2 more many2one fields for sales agent and buyer. added them to the form view for property listings form in a new notebook page called 'people'

--- odoo/addons/estate/models/estate_property.py
+++ odoo/addons/estate/models/estate_property.py
@@ -33,3 +33,5 @@ class EstateProperty(models.Model):
         default='new',
         help="The status of the property listing")
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    user_id = fields.Many2one('res.users', string='Sales Agent', index=True, tracking=True, default=lambda self: self.env.user)
+    buyer_id = fields.Many2one('res.partner', string='Buyer', copy=False, index=True, tracking=True)
